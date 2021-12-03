fb9ec0f57665aad6b9911b569dfe3539d7a27970
mohammed <mohammed.touab@gmail.com>
2021-12-03 17:05:50 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+import {
+  Home,
+  SingleProduct,
+  Cart,
+  About,
+  Products, Error,
+} from './pages'
+import {Footer, Navbar, Sidebar} from "./Components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+         <Router>
+           <Navbar />
+           <Sidebar />
+
+          <Switch>
+            <Route exact path='/'>
+              <Home />
+            </Route>
+            <Route exact path='/about'>
+              <About />
+            </Route>
+            <Route exact path='/cart'>
+              <Cart />
+            </Route>
+            <Route exact path='/products'>
+              <Products />
+            </Route>
+            <Route exact path='/products/:id' children={<SingleProduct />} />
+            <Route path="*">
+              <Error/>
+            </Route>
+
+          </Switch>
+           <Footer />
+
+         </Router>
+   )
 }
 
-export default App;
+export default App
---
+++ src/Components/index.js
@@ -0,0 +1,10 @@
+ import Navbar from './Navbar'
+ import Footer from './Footer'
+import Sidebar from './Sidebar'
+import CartButton from './CartButtons'
+export {
+   Navbar,
+   Footer,
+  Sidebar,
+  CartButton
+}
